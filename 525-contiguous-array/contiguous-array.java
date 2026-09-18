78fdf63cab45997a8501d839abf2ca50e6cef036
Paritosh125 <sandhanparitosh@gmail.com>
2026-09-18 20:00:14 +0530

    Time: 24 ms (53.10%) | Memory: 65.7 MB (25.45%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -4,7 +4,7 @@ class Solution {
         int zero = 0;
         int res = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,-1);
+        // map.put(0,-1);
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i] == 0)
@@ -12,6 +12,8 @@ class Solution {
             else
             one++;
             int diff= zero - one ;
+            if(diff == 0)
+            res = Math.max(res,i+1);
             if(!map.containsKey(diff)){
             map.put(diff ,i);
             }
