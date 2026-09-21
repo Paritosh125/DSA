5e1d52f4a8358b770968520724af51e27ec37c45
Paritosh125 <sandhanparitosh@gmail.com>
2026-09-21 19:34:57 +0530

    Time: 7 ms (98.94%) | Memory: 49.5 MB (5.85%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -5,23 +5,23 @@ class Solution {
 
         ArrayList<int[]> res = new ArrayList<>();
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
 
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
 
-            if (currStart <= end) {
-                end = Math.max(end, currEnd);
+            if (start2 <= end1) {
+                end1 = Math.max(end1, end2);
             } else {
-                res.add(new int[]{start, end});
-                start = currStart;
-                end = currEnd;
+                res.add(new int[]{start1, end1});
+                start1 = start2;
+                end1 = end2;
             }
         }
-        res.add(new int[]{start, end});
+        res.add(new int[]{start1, end1});
 
         return res.toArray(new int[res.size()][]);
     }
